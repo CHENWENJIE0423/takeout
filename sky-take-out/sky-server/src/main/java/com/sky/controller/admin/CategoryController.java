a113b9f034e1ddbcf4e88cba3a6220259dda1c51
Chen WenJie <CST2209140@xmu.edu.my>
2024-06-22 23:47:06 +0800

    完善分类板块业务代码

---
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,87 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author 陈文杰
+ * @project takeout
+ * @description
+ */
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+@Api(tags = "分类相关接口")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 查询分类
+     * @param type
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("查询分类")
+    public Result<List<Category>> getType(String type){
+        List<Category> category=categoryService.getType(type);
+        return Result.success(category);
+    }
+
+    /**
+     * 新增分类
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增分类")
+    public Result save(@RequestBody CategoryDTO categoryDTO){
+        categoryService.save(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO){
+        PageResult pageResult=categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @PutMapping
+    @ApiOperation("编辑分类")
+    public Result update(@RequestBody CategoryDTO categoryDTO){
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("修改状态")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        categoryService.startOrStop(status,id);
+        return Result.success();
+    }
+
+    @DeleteMapping
+    @ApiOperation("根据id删除员工")
+    public Result delete(Integer id){
+        categoryService.delete(id);
+        return Result.success();
+    }
+}
