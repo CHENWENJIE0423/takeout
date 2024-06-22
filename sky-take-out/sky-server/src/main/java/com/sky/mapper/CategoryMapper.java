69ab2b88762126db1df820e2287094b9b59cdd55
Chen WenJie <CST2209140@xmu.edu.my>
2024-06-22 23:47:06 +0800

    完善分类板块业务代码

---
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,44 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @author 陈文杰
+ * @project takeout
+ * @description
+ */
+@Mapper
+public interface CategoryMapper {
+
+    /**
+     * 根据type查询分类
+     * @param type
+     * @return
+     */
+    @Select("select * from category where type=#{type}")
+    List<Category> getByType(String type);
+
+    /**
+     * 插入分类
+     * @param category
+     */
+    @Insert("insert into category (type, name, sort, status, create_time, update_time, create_user, update_user) " +
+            "values (#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void insert(Category category);
+
+
+    Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    void update(Category category);
+
+    @Delete("delete from category where id=#{id}")
+    void delete(Integer id);
+}
---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -0,0 +1,27 @@
+package com.sky.service;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
+
+import java.util.List;
+
+/**
+ * @author 陈文杰
+ * @project takeout
+ * @description
+ */
+public interface CategoryService {
+    List<Category> getType(String type);
+
+    void save(CategoryDTO categoryDTO);
+
+    PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    void update(CategoryDTO categoryDTO);
+
+    void startOrStop(Integer status, Long id);
+
+    void delete(Integer id);
+}
